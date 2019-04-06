44f298fe61021db6979f74772f49ad571692a4fe
Kim Ying <kimying@ufl.edu>
2019-04-06 18:58:42 -0400

    Added the date to the logs so now we have the long milliseconds and a readable date listed out

--- src/floobits/Log.java
+++ src/floobits/Log.java
@@ -8,6 +8,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.time.Instant;
 import java.io.FileWriter;
+import java.util.Calendar;
 import java.util.Date;
 
 public class Log {
@@ -17,8 +18,8 @@ public class Log {
     }
 
     public static void toTextFile(IContext context, FloobitsState state, LogType type, String message) {
-        //String timestamp = Instant.now().toString();
         Long timestamp = Instant.now().toEpochMilli();
+        Date date = new Date();
         FlooUserDetail flooUserDetail = API.getUserDetail(context, state);
         String sep = " *|* ";
         try {
@@ -26,8 +27,8 @@ public class Log {
             //System.out.println("Working Directory = " + System.getProperty("user.dir"));
             // this file is being written to /Applications/IntelliJ IDEA CE.app/Contents/bin
             BufferedWriter writer = new BufferedWriter(new FileWriter("actionLogs.txt", true));
-            writer.write("\n" + timestamp + sep + type.name() + sep + flooUserDetail.username + sep + message);
-            System.out.println("\n" + timestamp + sep + type.name() + sep + flooUserDetail.username + sep + message);
+            writer.write("\n" + timestamp + sep + date + sep + type.name() + sep + flooUserDetail.username + sep + message);
+            System.out.println("\n" + timestamp + sep + date + sep + type.name() + sep + flooUserDetail.username + sep + message);
             writer.close(); //need to move this
         } catch (IOException ex) {
             System.out.println("caught an IOException");
@@ -35,12 +36,13 @@ public class Log {
     }
 
     public static void toTextFile(Date date, String username, LogType type, String message) {
+        Long timestamp = Instant.now().toEpochMilli();
         String sep = " *|* ";
         try {
             // this file is being written to /Applications/IntelliJ IDEA CE.app/Contents/bin
             BufferedWriter writer = new BufferedWriter(new FileWriter("actionLogs.txt", true));
-            writer.write("\n" + date + sep + type.name() + sep + username + sep + message);
-            System.out.println("\n" + date + sep + type.name() + sep + username + sep + message);
+            writer.write("\n" + timestamp + sep + date + sep + type.name() + sep + username + sep + message);
+            System.out.println("\n" + timestamp + sep + date + sep + type.name() + sep + username + sep + message);
             writer.close(); //need to move this
         } catch (IOException ex) {
             System.out.println("caught an IOException");
